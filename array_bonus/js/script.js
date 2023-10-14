18ff3dddec8119256cb4437ce2b274e97de10eb5
giuliabaratella <giuliabaratella7@gmail.com>
2023-10-14 11:42:11 +0200

    added cycle to calc arrays length

---
+++ array_bonus/js/script.js
@@ -0,0 +1,30 @@
+// 3.  (Bonus)
+// Cartella: array_bonus
+// Crea due array che hanno un numero di elementi diversi.
+// Aggiungi elementi all’array che ha meno elementi fino a quando ne avrà tanti quanti l’altro.
+
+// fare una comparazione tra l'indice delle due liste
+
+let arrayA = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let arrayB = [1, 2, 3]
+
+console.log (arrayA, arrayB);
+
+// calcolo la lunghezza dei due array 
+let nIndexA = 0 ;
+for (let i = 0; i < arrayA.length ; i++){
+    nIndexA ++;
+}
+console.log (nIndexA);
+
+let nIndexB = 0 ;
+for (let i = 0; i < arrayB.length ; i++){
+    nIndexB ++;
+}
+console.log (nIndexB);
+
+
+
+
+
+
